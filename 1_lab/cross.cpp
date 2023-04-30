3bed7e1b69dad29c7ec082174242a2b68b23985c
Stanislav <stas00333@gmail.com>
2023-05-01 02:16:11 +0300

    implement working multithreading
    Signed-off-by: Stanislav <stas00333@gmail.com>

--- 1_lab/cross.cpp
+++ 1_lab/cross.cpp
@@ -3,101 +3,160 @@
 #include <mpi.h>
 #include <fstream>
 #include <cassert>
+#include <memory.h>
 
 #include "cross.h"
 
 int main(int argc, char** argv) {
     MPI::Init(argc, argv);
+    double start_wtime = MPI::Wtime();
 
     auto rank = MPI::COMM_WORLD.Get_rank();
     auto commsize = MPI::COMM_WORLD.Get_size();
 
-    int distance = M / commsize;
-    int remainder = M % commsize;
-    int start = 1 + distance * rank;
-    int end = 1 + distance * (rank + 1);
+    // made constructor that allocate memory for different rank
+    Solution result(rank, commsize);
+    
+    result.FillInitialConditions();
+    result.CalculateFirstLineByRectangleMethod();
+    result.CalculateOtherLinesMultiWorkers();
 
-    // If the remainder is not zero, we add 1 term of the sum to each process starting from zero
-    if (remainder != 0) {
-        if (remainder - rank > 0) {
-            start += rank;
-            end += rank + 1;
-        }
-        else {
-            start += remainder;
-            end += remainder;
-        }
+    if (commsize > 1) {
+        result.GatheringAllWork();
     }
 
-    // made constructor that allocate memory for different rank
-    Solution result {};
-    
-    FillInitialConditions(&result);
-    CalculateFirstLineByRectangleMethod(&result);
-    CalculateOtherLines(&result);
+    double end_wtime = MPI::Wtime();
 
     if (rank == 0) {
+        std::cout << "[TIME = "<< end_wtime - start_wtime << "]" << std::endl;
+
         std::string name = "res.txt";
         std::ofstream f(name);
-        PrintRes(f, &result);
+        result.PrintRes(f);
     }
 
     MPI::Finalize();
     return 0;
 }
 
-void FillInitialConditions(Solution* result)
+void Solution::FillInitialConditions()
 {
-    for (int i = 0; i < K; ++i) {
-        result->t[i] = tau * i;
-        result->values[0 + i * M] = Equation::psi(tau * i);
-    }
-    for (int i = 0; i < M; ++i) {
-        result->x[i] = h * i;
-        result->values[i] = Equation::phi(h * i);
-    }
-}
-
-void CalculateFirstLineByRectangleMethod(Solution* result)
-{
-    double* values = result->values;
-
-    for (int m = 1; m < M; ++m) {
-        double first_part = (values[M + m - 1] - values[m - 1] - values[M + m]) / (2 * tau);
-        double second_part = Equation::a * (-1.0 * values[M + m - 1] + values[m]  - values[m - 1]) / (2 * h);
-        double func_part = Equation::func((m + 0.5) * h, (1 + 0.5) * tau);
-        values[M + m] = (func_part - first_part - second_part) * 2 * tau * h / (tau + h);
-    }
-}
-
-void CalculateOtherLines(Solution* result)
-{
-    double* values = result->values;
-
-    for (int k = 1; k < K - 1; ++k) {
-        for (int m = 1; m < M - 1; ++m) {
-            double first_part = (-1.0 * values[M * (k - 1) + m]) / (2 * tau);
-            double second_part = Equation::a * (values[M * k + m + 1] - values[M * k + m - 1]) / (2 * h);
-            double func_part = Equation::func(m * h, k * tau);
-            values[M * (k + 1) + m] = (func_part - first_part - second_part) * 2 * tau;
+    if (rank_ == 0) {
+        for (int i = 0; i < K_; ++i) {
+            values_[0 + i * cols_] = Equation::psi(tau * i);
         }
-        // code for m = M - 1
-        double first_part = (values[M * (k + 1) + M - 1 - 1] - values[M * k + M - 1 - 1] - values[M * k + M - 1]) / (2 * tau);
-        double second_part = Equation::a * (-1.0 * values[M * (k + 1) + M - 1 - 1] + values[M * k + M - 1] - values[M * k + M - 1 - 1]) / (2 * h);
-        double func_part = Equation::func((M - 1 + 0.5) * h, (k + 0.5) * tau);
-        values[M * (k + 1) + M - 1] = (func_part - first_part - second_part) * 2 * tau * h / (tau + h);
+        for (int i = 0; i < cols_; ++i) {
+            values_[i] = Equation::phi(h * i);
+        }
+    } else {
+        for (int i = 0; i < cols_; ++i) {
+            values_[i] = Equation::phi(h * (i + start_pos_));
+        }
     }
 }
 
-void PrintRes(std::ostream &ost, Solution* result)
+void Solution::CalculateFirstLineByRectangleMethod()
+{
+    if (rank_ == 0) {
+        for (int m = 1; m < cols_; ++m) {
+            double first_part = (values_[cols_ + m - 1] - values_[m - 1] - values_[m]) / (2 * tau);
+            double second_part = Equation::a * (-1.0 * values_[cols_ + m - 1] + values_[m]  - values_[m - 1]) / (2 * h);
+            double func_part = Equation::func((m + 0.5) * h, (1 + 0.5) * tau);
+            values_[cols_ + m] = (func_part - first_part - second_part) * 2 * tau * h / (tau + h);
+        }
+        if (commsize_ > 1) {
+            MPI_Send(values_ + cols_ + cols_ - 1, 1, MPI::DOUBLE, rank_ + 1, 0, MPI_COMM_WORLD);
+            MPI_Send(values_ + cols_ - 1, 1, MPI::DOUBLE, rank_ + 1, 0, MPI_COMM_WORLD);
+        }
+    } else {
+        double recv_up_value = 0;
+        double recv_down_value = 0;
+        MPI_Status status;
+        MPI_Recv(&recv_up_value, 1, MPI::DOUBLE, rank_ - 1, 0, MPI_COMM_WORLD, &status);
+        MPI_Recv(&recv_down_value, 1, MPI::DOUBLE, rank_ - 1, 0, MPI_COMM_WORLD, &status);
+        
+        for (int m = 0; m < cols_; ++m) {
+            if (m != 0) {
+                recv_up_value = values_[cols_ + m - 1];
+                recv_down_value = values_[m - 1];
+            }
+            double first_part = (recv_up_value - recv_up_value - values_[m]) / (2 * tau);
+            double second_part = Equation::a * (-1.0 * recv_up_value + values_[m]  - recv_down_value) / (2 * h);
+            double func_part = Equation::func((start_pos_ + m + 0.5) * h, (1 + 0.5) * tau);
+            values_[cols_ + m] = (func_part - first_part - second_part) * 2 * tau * h / (tau + h);
+        }
+        if (rank_ != commsize_ - 1) {
+            MPI_Send(values_ + cols_ + cols_ - 1, 1, MPI::DOUBLE, rank_ + 1, 0, MPI_COMM_WORLD);
+            MPI_Send(values_ + cols_ - 1, 1, MPI::DOUBLE, rank_ + 1, 0, MPI_COMM_WORLD);
+        }
+    }
+}
+
+void Solution::CalculateOtherLinesMultiWorkers()
+{
+    for (int k = 1; k < K_ - 1; ++k) {
+        double recv_value = 0; 
+        if (rank_ != 0) {
+            MPI_Status status;
+            MPI_Recv(&recv_value, 1, MPI::DOUBLE, rank_ - 1, 0, MPI_COMM_WORLD, &status);
+        }
+        // if rank = 0 we dont need to calculate m = 0
+        for (int m = (rank_ == 0); m < cols_ - 1; ++m) {
+            if (m != 0) {
+                recv_value = values_[cols_ * k + m - 1];
+            }
+            double first_part = (-1.0 * values_[cols_ * (k - 1) + m]) / (2 * tau);
+            double second_part = Equation::a * (values_[cols_ * k + m + 1] - recv_value) / (2 * h);
+            double func_part = Equation::func((start_pos_ + m) * h, k * tau);
+            values_[cols_ * (k + 1) + m] = (func_part - first_part - second_part) * 2 * tau;
+        }
+        int m = cols_ - 1;
+        double first_part = (values_[cols_ * (k + 1) + m - 1] - values_[cols_ * k + m - 1] - values_[cols_ * k + m]) / (2 * tau);
+        double second_part = Equation::a * (-1.0 * values_[cols_ * (k + 1) + m - 1] + values_[cols_ * k + m] - values_[cols_ * k + m - 1]) / (2 * h);
+        double func_part = Equation::func((start_pos_ + m + 0.5) * h, (k + 0.5) * tau);
+        values_[cols_ * (k + 1) + m] = (func_part - first_part - second_part) * 2 * tau * h / (tau + h);
+
+        if (rank_ != commsize_ - 1) {
+            MPI_Send(values_ + cols_ * k + m, 1, MPI::DOUBLE, rank_ + 1, 0, MPI_COMM_WORLD);
+        }
+    }
+}
+
+void Solution::PrintRes(std::ostream &ost)
 {
     ost << "tau: " << tau << std::endl;
     ost << "h: " << h << std::endl;
     ost << "T: " << Equation::T << std::endl;
     ost << "X: " << Equation::X << std::endl;
-    ost << "M: " << M << std::endl;
-    ost << "K: " << K << std::endl;
+    ost << "M: " << M_ << std::endl;
+    ost << "K: " << K_ << std::endl;
 
-    for (std::size_t i = 0, sz = K * M; i < sz; ++i)
-        ost << result->values[i] << std::endl;
+    double* array_to_print = nullptr;
+    if (commsize_ == 1) {
+        array_to_print = values_;
+    } else {
+        array_to_print = result_values_;
+    }
+
+    for (std::size_t i = 0, sz = K_ * M_; i < sz; ++i)
+        ost << array_to_print[i] << std::endl;
+}
+
+void Solution::CalculateStartAndEndPos()
+{
+    int distance = M_ / commsize_;
+    int remainder = M_ % commsize_;
+    if (remainder != 0) {
+        M_ = (distance + 1) * commsize_;
+        ++distance;
+    }
+    start_pos_ = distance * rank_;
+    end_pos_ = distance * (rank_ + 1);
+}
+
+void Solution::GatheringAllWork()
+{    
+    for (int k = 0; k < K_; ++k) {
+        MPI_Gather(values_ + k * cols_, cols_, MPI::DOUBLE, result_values_ + k * M_, cols_, MPI::DOUBLE, 0, MPI_COMM_WORLD);
+    }
 }
--- 1_lab/cross.h
+++ 1_lab/cross.h
@@ -3,57 +3,101 @@
 
 #include <array>
 #include <vector>
+#include <cmath>
 
 namespace Equation {
 
 constexpr double a = 1.0;
-constexpr double X = 10.0;
-constexpr double T = 10.0;
+constexpr double X = 1.0;
+constexpr double T = 1.0;
 
 double func(double x, double t)
 { 
-    return x * t;
+    return t + x;
 }
 
 double phi(double x)
 {
-    return x;
+    return std::cos(M_PI * x);
 }
 
 double psi(double t)
 {
-    return -1.0 * t;
+    return std::exp(-t);
 }
 
 }; // namespace Equation
 
+// For the method to be stable, Courant number must be less than 1
+// a * tau / h < 1
 constexpr double h = 0.05;
 constexpr double tau = 0.001;
-constexpr int K = Equation::T / tau + 1;
-constexpr int M = Equation::X / h + 1;
+constexpr long int K = Equation::T / tau;
+constexpr long int M = Equation::X / h;
 
-struct Solution {
-    Solution()
+class Solution {
+public:
+    Solution(int rank, int commsize)
     {
-        values = new double[K * M];
-        t = new double[K];
-        x = new double[M];
+        rank_ = rank;
+        commsize_ = commsize;
+
+        CalculateStartAndEndPos();
+        cols_ = end_pos_ - start_pos_;
+        values_ = new double[cols_ * K_] {};
+
+        if (rank_ == 0) {
+           result_values_ = new double[M_ * K_] {}; 
+        }
     }
     ~Solution()
     {
-        delete[] values;
-        delete[] t;
-        delete[] x;
+        delete[] values_;
+        if (rank_ == 0) {
+            delete[] result_values_;
+        }
     }
 
-    double* values {nullptr};
-    double* t {nullptr};
-    double* x {nullptr};
-};
+    void FillInitialConditions();
+    void CalculateFirstLineByRectangleMethod();
+    void CalculateOtherLines();
+    void CalculateOtherLinesSingleWorker();
+    void CalculateOtherLinesMultiWorkers();
+    void GatheringAllWork();
+    void PrintRes(std::ostream &ost);
 
-void FillInitialConditions(Solution* result);
-void CalculateFirstLineByRectangleMethod(Solution* result);
-void CalculateOtherLines(Solution* result);
-void PrintRes(std::ostream &ost, Solution* result);
+private:
+    int rank_ {0};
+    int commsize_ {0};
+    int start_pos_ {0};
+    int end_pos_ {0};
+    int cols_ {0};
+
+    int M_ {M};
+    int K_ {K};
+
+    double* values_ {nullptr};
+    double* result_values_ {nullptr};
+
+    void CalculateStartAndEndPos();
+    /**   Example for M = 5
+     *
+     *    [0, 2), [2, 4), [4, 5)
+     *     |                  |
+     *     start_pos          end_pos
+     *
+     *
+     *    0 + + + +
+     *    0 0 0 0 +
+     *    0 0 0 0 0
+     *    0 0 0 0 0
+     *    ---------
+     *    0 1 2 3 4
+     *            |
+     *            If pos == M - 1 we should use Rectangle method
+     *            So when end_pos == M, we can be sure that it is the last worker
+     *   
+    **/     
+};
 
 #endif  // CROSS_H
